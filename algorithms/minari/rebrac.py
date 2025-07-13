1df787a8ac6ac1e7a44fd2838c54d82638d307fb
Alex Davey <alexdavey0@gmail.com>
2025-07-13 18:20:53 +0200

    Update REBRAC

--- algorithms/minari/rebrac.py
+++ algorithms/minari/rebrac.py
@@ -200,14 +200,40 @@ class EnsembleCritic(nn.Module):
         return q_values
 
 
+# Convert GoalEnv dict observation to array [pos_xy, vector_to_goal, other_obs]
+def process_goalenv_obs(obs):
+    return np.concatenate([
+        obs["achieved_goal"],
+        obs["desired_goal"] - obs["achieved_goal"],
+        obs["observation"]
+    ], axis=-1)
+
+
+def wrap_goalenv(env):
+    achieved_goal = env.observation_space["achieved_goal"]
+    desired_goal = env.observation_space["desired_goal"]
+    observation = env.observation_space["observation"]
+    direction_low = desired_goal.low - achieved_goal.high
+    direction_high = desired_goal.high - achieved_goal.low
+    new_obs_space = gym.spaces.Box(
+        low=np.concatenate([achieved_goal.low, direction_low, observation.low]),
+        high=np.concatenate([achieved_goal.high, direction_high, observation.high])
+    )
+    env = gym.wrappers.TransformObservation(env, process_goalenv_obs, new_obs_space)
+    return env
+
+
 # TODO: Double check off-by-one
 # WARN: this will load full dataset in memory (which is OK for D4RL datasets)
 def qlearning_dataset(dataset: minari.MinariDataset) -> Dict[str, np.ndarray]:
     obs, next_obs, actions, next_actions, rewards, dones = [], [], [], [], [], []
 
     for episode in dataset:
-        obs.append(episode.observations[:-1].astype(np.float32))
-        next_obs.append(episode.observations[1:].astype(np.float32))
+        ep_obs = episode.observations
+        if isinstance(episode.observations, dict):
+            ep_obs = process_goalenv_obs(ep_obs)
+        obs.append(ep_obs[:-1].astype(np.float32))
+        next_obs.append(ep_obs[1:].astype(np.float32))
         actions.append(episode.actions[:-1].astype(np.float32))
         next_actions.append(episode.actions[1:].astype(np.float32))
         rewards.append(episode.rewards[:-1])
@@ -327,6 +353,8 @@ def normalize(
 
 def make_env(minari_dataset: minari.MinariDataset, seed: int) -> gym.Env:
     env = minari_dataset.recover_environment(eval_env=True)
+    if isinstance(env.observation_space, gym.spaces.Dict):
+        env = wrap_goalenv(env)
     env.action_space.seed(seed)
     env.observation_space.seed(seed)
     return env
