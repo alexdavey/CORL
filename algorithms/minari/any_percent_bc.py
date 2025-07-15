ae4d70b84d9791f2356fd9dad7dd079cfa345dd2
Alex Davey <alexdavey0@gmail.com>
2025-07-16 00:34:58 +0200

    Update any_percent_bc.py

--- algorithms/minari/any_percent_bc.py
+++ algorithms/minari/any_percent_bc.py
@@ -341,7 +341,7 @@ class BC:
 @pyrallis.wrap()
 def train(config: TrainConfig):
     minari_dataset = minari.load_dataset(config.env, download=True)
-    env = minari_dataset.recover_environment(eval_env=True)
+    env = minari_dataset.recover_environment()
 
     if isinstance(env.observation_space, gym.spaces.Dict):
         env = wrap_goalenv(env)
